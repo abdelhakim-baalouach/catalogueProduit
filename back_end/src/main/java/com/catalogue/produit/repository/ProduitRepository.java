46a172961aec64863cffbf996e50b7968adce820
Your Name <you@example.com>
2020-07-07 13:38:01 +0100

    front end 2

--- back_end/src/main/java/com/catalogue/produit/repository/ProduitRepository.java
+++ back_end/src/main/java/com/catalogue/produit/repository/ProduitRepository.java
@@ -4,6 +4,6 @@ import com.catalogue.produit.model.Produit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:4200")
 public interface ProduitRepository  extends JpaRepository<Produit, Long> {
 }
